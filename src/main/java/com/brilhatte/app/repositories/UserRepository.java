8400effc2edfd802518186890bf4596662df3501
bruno-venturini <brunofreloventurini@outlook.com>
2025-03-09 17:35:50 -0300

    ajuste UserRepository

--- src/main/java/com/brilhatte/app/repositories/UserRepository.java
+++ src/main/java/com/brilhatte/app/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.brilhatte.app.repositories;
 import com.brilhatte.app.models.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     UserDetails findByUsername(String username);
 }
